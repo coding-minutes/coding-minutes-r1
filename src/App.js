9674f7b542ff0df3a8303b2e41188ac3563d2a49
Sarveshwar Shukla <sarveshwarshukla27@gmail.com>
2022-05-15 12:26:28 +0530

    Added coupon code BDAY360

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import React, {useState, useEffect} from 'react'
-import Navbar from './components/navbar/Navbar'
-import Header from './components/header/Header'
-import Statistics from './components/statistics/Statistics'
-import LearningPath from './components/learning-paths/LearningPaths'
-import Reviews from './components/reviews/Reviews'
-import WhyCM from './components/why-cm/WhyCM'
-import CampusCaptains from './components/campus-captains/CampusCaptains'
-import Team from './components/team/Team'
-import FAQ from './components/faq/FAQ'
-import Footer from './components/footer/Footer'
+import React from 'react'
 
-import Popup from './components/popup/Popup'
 import Home from './components/Home'
 import CampusCaptainForm from './components/campus-captains-form/CampusCaptainForm'
-import { BrowserRouter as Router, Route, Link, NavLink, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Discord from './components/discord/Discord'
 
 
 const App = () => {
 
-    const [openModal, setOpenModal] = useState(false);
 
-    useEffect(() =>{
-        setTimeout(() =>{
-            setOpenModal(true);
-        }, 5000);
-    }, [])
 
     return (
         <>
