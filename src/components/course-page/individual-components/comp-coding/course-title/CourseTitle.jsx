703d375683392086dd626cd3178227b17d2bc091
Sarveshwar Shukla <sarveshwarshukla27@gmail.com>
2022-06-10 11:53:54 +0530

    updated code JUNE360 from JUNE420

--- src/components/course-page/individual-components/comp-coding/course-title/CourseTitle.jsx
+++ src/components/course-page/individual-components/comp-coding/course-title/CourseTitle.jsx
@@ -16,12 +16,12 @@ const CourseTitle = () => {
               Competitive Programming Essentials, Master Algorithms Course is now on #Udemy!
             </div>
             <div className="price-info">
-              <span className="first">₹ 420</span>
+              <span className="first">₹ 360</span>
               <span className="middle">₹ 18,560</span>
-              <span className="last">Use code : JUNE420</span>
+              <span className="last">Use code : JUNE360</span>
             </div>
             <div className="udemy-info">
-              <a href="https://www.udemy.com/course/competitive-programming-algorithms-coding-minutes/?couponCode=JUNE420"  target="_blank">Enroll now on Udemy</a>
+              <a href="https://www.udemy.com/course/competitive-programming-algorithms-coding-minutes/?couponCode=JUNE360"  target="_blank">Enroll now on Udemy</a>
               <span className="stars">⭐⭐⭐⭐⭐ 4.88/5.0</span>
             </div>
           </div>
