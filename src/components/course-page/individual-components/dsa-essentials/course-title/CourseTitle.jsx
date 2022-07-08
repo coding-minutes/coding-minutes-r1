1e02d6d8c9d0beb0b90478a374e32c1a29920a58
Sarveshwar Shukla <sarveshwarshukla27@gmail.com>
2022-07-08 09:22:50 +0530

    updated coupon code to JULY420

--- src/components/course-page/individual-components/dsa-essentials/course-title/CourseTitle.jsx
+++ src/components/course-page/individual-components/dsa-essentials/course-title/CourseTitle.jsx
@@ -19,10 +19,10 @@ const CourseTitle = () => {
             <div className="price-info">
               <span className="first">₹ 420</span>
               <span className="middle">₹ 18,560</span>
-              <span className="last">Use code : JUNE420</span>
+              <span className="last">Use code : JULY420</span>
             </div>
             <div className="udemy-info">
-              <a href="https://www.udemy.com/course/cpp-data-structures-algorithms-prateek-narang/?couponCode=JUNE420"  target="_blank">Enroll now on Udemy</a>
+              <a href="https://www.udemy.com/course/cpp-data-structures-algorithms-prateek-narang/?couponCode=JULY420"  target="_blank">Enroll now on Udemy</a>
               <span className="stars">⭐⭐⭐⭐⭐ 4.88/5.0</span>
             </div>
           </div>
