60bc4387f9a8732d993610a23fe70ca73f335822
Sarveshwar Shukla <sarveshwarshukla27@gmail.com>
2022-09-08 16:49:16 +0530

    updated coupon code to sept440 from aug440

--- src/components/course-page/individual-components/comp-coding/course-title/CourseTitle.jsx
+++ src/components/course-page/individual-components/comp-coding/course-title/CourseTitle.jsx
@@ -21,10 +21,10 @@ const CourseTitle = () => {
             <div className="price-info">
             <span className="first">₹ 440+taxes</span>
               <span className="middle">₹ 18,560</span>
-              <span className="last">Use code : AUG440</span>
+              <span className="last">Use code : SEPT440</span>
             </div>
             <div className="udemy-info">
-              <a href="https://www.udemy.com/course/competitive-programming-algorithms-coding-minutes/?couponCode=AUG440"  target="_blank">Enroll now on Udemy</a>
+              <a href="https://www.udemy.com/course/competitive-programming-algorithms-coding-minutes/?couponCode=SEPT440"  target="_blank">Enroll now on Udemy</a>
               <span className="stars">⭐⭐⭐⭐⭐ 4.88/5.0</span>
             </div>
           </div>
--- src/components/course-page/individual-components/python-for-data-science/buy-on-udemy/BuyOnUdemy.jsx
+++ src/components/course-page/individual-components/python-for-data-science/buy-on-udemy/BuyOnUdemy.jsx
@@ -13,7 +13,7 @@ const BuyOnUdemy = () => {
             <h3>Ready to take your</h3>
             <h3>Coding Skills to Next Level?</h3>
             <p>Join the Course Now!</p>
-            <a href="https://www.udemy.com/course/python-data-science-master-course/?couponCode=AUG440"  target="_blank">
+            <a href="https://www.udemy.com/course/python-data-science-master-course/?couponCode=SEPT440"  target="_blank">
               Enroll now on Udemy
             </a>
           </div>
