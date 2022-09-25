04c78d67fc4289d6b4d38132fa6b0d66b8342c7b
Sarveshwar Shukla <sarveshwarshukla27@gmail.com>
2022-09-25 10:44:58 +0530

    added array of images on headerbanner to change it at certain interval

--- src/components/header-banner/HeaderBanner.jsx
+++ src/components/header-banner/HeaderBanner.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './headerBanner.css'
-import freedombanner from '../../assets/Images/main/engineer-day-banner.png'
+import freedombanner from '../../assets/Images/main/sept440-banner.png'
 import freedombannerphone from '../../assets/Images/main/freedombannerphone.png'
 import headerBannerImage from '../../assets/Images/main/headerBanner.svg'
 import headerBannerPhoneImage from '../../assets/Images/main/headerbannerphone.png'
+import headerBanner1 from '../../assets/Images/main/sept440-banner-bg-orange.svg'
+import headerBanner2 from '../../assets/Images/main/sept440-banner-bg-purple.svg'
 
+const banners = [headerBanner1, headerBanner2];
 const HeaderBanner = () => {
+
+    const [currentImage, setCurrentImage] = useState(headerBanner1);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImage(banners[Math.floor(Math.random() * banners.length)])
+        }, 5000)
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <div className="components-outer-container">
             <a href="#courses">
                 <div className="header-banner-container">
-                    <img className="headerImage" src={freedombanner} alt="" />
-                    <img className="headerImagePhone" src={freedombanner} alt="" />
+                    <img className="headerImage header-banner-img" src={currentImage} alt="" />
+                    {/* <img className="headerImagePhone" src={headerBannerPhoneImage} alt="" /> */}
+
                 </div>
             </a>
         </div>
