8f6fdeaea1077209f3ecc235421b686b157238aa
Utkarsh Dwivedi <68611209+Utkarsh742@users.noreply.github.com>
2022-12-05 22:04:56 +0530

    Updated coupon code

--- src/components/course-page/individual-components/python-for-data-science/course-title/CourseTitle.jsx
+++ src/components/course-page/individual-components/python-for-data-science/course-title/CourseTitle.jsx
@@ -21,10 +21,10 @@ const CourseTitle = () => {
             <div className="price-info">
             <span className="first">₹ 3,499</span>
               <span className="middle">₹ 18,560</span>
-              <span className="last">Sale starts soon!!</span>
+              <span className="last">DEC440</span>
             </div>
             <div className="udemy-info">
-              <a href="https://www.udemy.com/course/python-data-science-master-course/?couponCode=BLACKFRIDAY360"  target="_blank">Enroll now on Udemy</a>
+              <a href="https://www.udemy.com/course/python-data-science-master-course/?couponCode=DEC440"  target="_blank">Enroll now on Udemy</a>
               <span className="stars">⭐⭐⭐⭐⭐ 4.88/5.0</span>
             </div>
           </div>
