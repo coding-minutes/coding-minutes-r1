6760001705a1d8da1540b008d406db4094b4654b
GOVIND GUPTA <govind030303@gmail.com>
2022-12-30 19:43:17 +0530

    Social Links of the team updated

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -6,9 +6,9 @@ export const teamData = [
         image: prateekBhaiya,
         description1: 'Prateek is a popular programming instructor and an ace software engineer, currently working with Scaler and created Coding Minutes. He is known for his amazingly simplified explanations that make everyone fall in love with programming.',
         description2: 'He has over 5+ years of teaching experience and has trained over 75,000 students in classroom boot camps & online courses in the past.',
-        linkedin: 'https://github.com',
-        instagram: 'https://www.instagram',
-        youtube: 'https://www.youtube',
+        linkedin: 'https://www.linkedin.com/in/prateeknarang27/',
+        instagram: 'https://www.instagram.com/prateeknarang27/',
+        youtube: 'https://www.youtube.com/@PrateekNarang27',
 
     },
     {
@@ -16,9 +16,9 @@ export const teamData = [
         image: mohitBhaiya,
         description1: 'Mohit is a Data Scientist programming instructor and co-creator at Coding Minutes. He has trained over 10000+ students in Machine Learning, and AI over the last 2 years of his teaching experience. His expertise lies in Python, data science, machine learning and AI, and he has won many competitions.',
         description2: 'He has been doing AI-based projects for the last 4 years. He also leads the ML and Data Science Domain of Coding Minutes.',
-        linkedin: 'https://github.com',
-        instagram: 'https://www.instagram',
-        youtube: 'https://www.youtube',
+        linkedin: 'https://www.linkedin.com/in/mohituniyal/',
+        instagram: 'https://www.instagram.com/mohituniyal2010/',
+        youtube: 'https://www.youtube.com/@CodingMinutes',
     },
     {
         id: 3,
