f3bed04912bf29a53e07aeadd34e99752f22d628
Utkarsh Dwivedi <68611209+Utkarsh742@users.noreply.github.com>
2023-02-23 09:00:56 +0530

    Updated Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import './header.css'
 import { headerImage } from '../../assets'
 
@@ -10,15 +10,21 @@ const Header = () => {
         <div className="header-container">
           <div className="left" data-aos="fade-right">
             <div className="first">
-              <h1>Kickstart your</h1>
-              <h1 className="cm-gradient-text">Coding Career in Minutes</h1>
+              {/* <h1>Kickstart your</h1> */}
+              <h2 className="newly-launched">🔥NEW COURSE LAUNCH🔥</h2>
+              <h1>Machine Learning Essentials</h1>
+              {/* <h1 className="cm-gradient-text">Coding Career in Minutes</h1> */}
+              <h1 className="cm-gradient-text">Get earlybird 90% off!</h1>
             </div>
-            <div className="second">
+            {/* <div className="second">
               <p><strong>Pocket friendly courses</strong> with interactive content, top-notch
-                instructors, TA support, Projects & Assignments.</p>
+                instructors, TA support, Projects & Assignments.</p> */}
+            <div className="second">
+              <p><strong>28 hours of immersive content</strong> with 8+ hands-on industry
+                projects with lifetime access!</p>
             </div>
             <div className="third">
-              <button className="cm-gradient-button"><a href="#courses">Explore Courses</a></button>
+              <button className="cm-gradient-button"><Link to={`/courses/machine-learning-essentials`} >Get Earlybird 90% OFF</Link></button>
             </div>
           </div>
           <div className="right" data-aos="fade-left">
