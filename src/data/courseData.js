6929d1cb05f4165ba13a0745f24067f10df28d47
Prateek Narang <prateeknarang111@gmail.com>
2023-10-04 11:07:30 +0530

    Update courseData.js

--- src/data/courseData.js
+++ src/data/courseData.js
@@ -2,8 +2,8 @@ import { codingEssentials, competitiveProgramming, computerVisionSpecialization,
 import { teamData } from "./teamData"
 import { whyCMData } from './whyCMData'
 
-export const couponCode = "FREEDOM23";
-const currentPrice = "₹500+taxes ";
+export const couponCode = "OCTSALE23";
+const currentPrice = "₹495+taxes ";
 
 const cppHindiCourse = {
   courseImage: cppHindi,
